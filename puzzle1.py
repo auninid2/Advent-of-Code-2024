2f734b89c8a54b63b29a0d1cc46081cf15bf4793
ae3101fu <ae3101fu@mi.fu-berlin.de>
2025-07-25 17:14:37 +0200

    Day 1

---
+++ puzzle1.py
@@ -0,0 +1,30 @@
+# ---------------------------------
+# --- Day 1: Historian Hysteria ---
+# ---------------------------------
+
+from collections import Counter
+
+list1 = []
+list2 = []
+
+with open("input.txt", "r") as file:
+    acc = 0
+
+    for line in file:
+        for word in line.split():
+            if acc % 2 == 0:
+                list1.append(int(word))
+            else: 
+                list2.append(int(word))
+            acc += 1
+
+list1.sort()
+list2.sort()
+
+total_distance = sum(abs(a - b) for a, b in zip(list1, list2))
+
+counter = Counter(list2)
+similarity_score = sum(counter[x] * x for x in list1)
+
+print("Total distance between both lists:", total_distance)
+print("Similarity score beetwen both lists:", similarity_score)
