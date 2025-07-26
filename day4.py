6890fca6144fe62ae72e88b622b38717f38d65a2
ae3101fu <ae3101fu@mi.fu-berlin.de>
2025-07-26 15:49:04 +0200

    Ceres Search

--- day4.py
+++ day4.py
@@ -1,6 +1,6 @@
-# ---------------------------------
-# ------ Day 4: Ceres Search ------
-# ---------------------------------
+# -------------------------------
+# ----- Day 4: Ceres Search -----
+# -------------------------------
 
 with open("inputs/day4_input.txt") as file:
     matrix = [list(line.strip()) for line in file]
@@ -8,64 +8,67 @@ with open("inputs/day4_input.txt") as file:
 n = len(matrix)
 m = len(matrix[0])
 
-def search_xmas(matrix):
-    total = 0
-    word = "XMAS"
-    directions = [  
-        (-1, 0),  # above
-        (1, 0),   # below
-        (0, -1),  # left
-        (0, 1),   # right
-        (-1, -1), # diagonal above left
-        (-1, 1),  # diagonal above right
-        (1, -1),  # diagonal below left
-        (1, 1),   # diagonal below right
-    ]
+WORD_XMAS = "XMAS"
+X_MAS_PATTERNS = ["MAS", "SAM"]
+
+DIRECTIONS = [  
+    (-1, 0),  # up
+    (1, 0),   # down
+    (0, -1),  # left
+    (0, 1),   # right
+    (-1, -1), # diag up-left
+    (-1, 1),  # diag up-right
+    (1, -1),  # diag down-left
+    (1, 1),   # diag down-right
+]
+
+# --- Part One ---
+
+def count_word_occurrences(matrix, word):
+    count = 0
+    word_len = len(word)
 
     for i in range(n):
         for j in range(m):
-            for dx, dy in directions:
-                try:
-                    if all(
-                        0 <= i + k*dx < n and 
-                        0 <= j + k*dy < m and 
-                        matrix[i + k*dx][j + k*dy] == word[k]
-                        for k in range(len(word))
-                    ):
-                        total += 1
-                except IndexError:
-                    continue
+            for dx, dy in DIRECTIONS:
+                if all(
+                    0 <= i + k*dx < n and
+                    0 <= j + k*dy < m and
+                    matrix[i + k*dx][j + k*dy] == word[k]
+                    for k in range(word_len)
+                ):
+                    count += 1
+    return count
 
-    return total
+# --- Part Two ---
 
-def search_x_mas(matrix):
-    total = 0
+def count_x_mas_patterns(matrix):
+    count = 0
 
-    def check_diagonal(line, col, dx1, dy1, dx2, dy2, pattern):
-        try:
-            return (
-                matrix[line + dx1][col + dy1] == pattern[0] and
-                matrix[line][col] == pattern[1] and
-                matrix[line + dx2][col + dy2] == pattern[2]
-            )
-        except IndexError:
-            return False
+    def match_diag(i, j, pattern, dx1, dy1, dx2, dy2):
+        return (
+            0 <= i + dx1 < n and 0 <= j + dy1 < m and
+            0 <= i + dx2 < n and 0 <= j + dy2 < m and
+            matrix[i + dx1][j + dy1] == pattern[0] and
+            matrix[i][j] == pattern[1] and
+            matrix[i + dx2][j + dy2] == pattern[2]
+        )
 
     for i in range(1, n - 1):
         for j in range(1, m - 1):
             if matrix[i][j] != 'A':
                 continue
 
-            diag1_MAS = check_diagonal(i, j, -1, -1, 1, 1, "MAS")
-            diag1_SAM = check_diagonal(i, j, -1, -1, 1, 1, "SAM")
+            diag1_matches = any(
+                match_diag(i, j, pattern, -1, -1, 1, 1) for pattern in X_MAS_PATTERNS
+            )
+            diag2_matches = any(
+                match_diag(i, j, pattern, -1, 1, 1, -1) for pattern in X_MAS_PATTERNS
+            )
 
-            diag2_MAS = check_diagonal(i, j, -1, 1, 1, -1, "MAS")
-            diag2_SAM = check_diagonal(i, j, -1, 1, 1, -1, "SAM")
+            if diag1_matches and diag2_matches:
+                count += 1
+    return count
 
-            if (diag1_MAS or diag1_SAM) and (diag2_MAS or diag2_SAM):
-                total += 1
-
-    return total
-
-print("XMAS appears a total of", search_xmas(matrix), "times")
-print("X-MAS appears a total of", search_x_mas(matrix), "times")
+print("XMAS appears a total of", count_word_occurrences(matrix, WORD_XMAS), "times")
+print("X-MAS appears a total of", count_x_mas_patterns(matrix), "times")
